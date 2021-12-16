434f17cdb18f7b2d6219e8b53c417f7b8a923c5f
nitsolution2021 <praveen@nitsolution.in>
2021-12-16 17:13:01 +0530

    created Bean Class and Service Class for Product

---
+++ src/app/common/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    inutsInStock: string;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+
+}

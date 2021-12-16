35d009a077554def2cbed9f5d8c4312d863275e3
nitsolution2021 <praveen@nitsolution.in>
2021-12-16 18:08:53 +0530

    Integrate Angular and SpringBoot

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProdcts();
+  }
+  
+  listProdcts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
